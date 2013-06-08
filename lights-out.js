bae7e519833c4e93efc46ce0bb65b3d5e718cf9c
Marshall <mad.mod.max@gmail.com>
2013-06-07 23:28:11 -0400

    We're on a roll here! First level completed.
    (almost)

--- lights-out.js
+++ lights-out.js
@@ -19,70 +19,25 @@ $(document).ready(function() {
 		}
 	});
 
-	Q.Sprite.extend("Tile", {
-		init: function(p) {
-			this._super(p, {
-				asset: "tile.png",
-				x: 0,
-				y: 0
-			});
-		}
-	});
-
-	Q.Sprite.extend("AirTile", {
-		init: function(p) {
-			this._super(p, {
-				asset: "air-tile.png",
-				x: 0,
-				y: 0
-			});
-
-		}
-	});
-
-	Q.scene("testlevel", function(stage) {
-		stage.add("viewport");
-
+	Q.scene("level-one", function(stage) {
 		stage.insert(new Q.Repeater({ asset: "background.png", speedX: 0.5, speedY: 0.5}));
 
 		stage.collisionLayer(new Q.TileLayer({
-			dataAsset: "testlevel.json",
+			dataAsset: "level-one.json",
 			sheet: "tiles"
 		}));
 
 		var player = stage.insert(new Q.Player());
-		//var tiles = [];
-		//var AirTile = [];
-		
-		// for (var i = 0; i < 30; i++) {
-		// 	stage.insert(new Q.Tile({
-		// 		x: 16 + 32 * i,
-		// 		y: W.height() - 16
-		// 	}));
-		// }
 
-		// for (var i = 0; i < 18; i++) {
-		// 	stage.insert(new Q.AirTile({
-		// 		x: W.width() - 16,
-		// 		y: 8 + 32 * i
-		// 	}));
-
-		// 	stage.insert(new Q.AirTile({
-		// 		x: 16,
-		// 		y: 8 + 32 * i
-		// 	}));
-		// }
-
-		//stage.add("viewport").follow(player, { x: true, y: false })''
-
-		//stage.add("viewport");
+		stage.add("viewport").follow(player, { x: true, y: true });
+		//stage.viewport.scale = 2;
 	});
 
-	Q.load("player.png, tiles.png, testlevel.json, background.png", function() {
+	Q.load("player.png, tiles.png, level-one.json, background.png", function() {
 		Q.sheet("tiles", "tiles.png", { tilew: 32, tileh: 32 });
 
-		//Q.compileSheets("tiles.png", "testlevel.json");
+		Q.compileSheets("tiles.png", "testlevel.json");
 
-		Q.stageScene("testlevel");
+		Q.stageScene("level-one");
 	});
 });
