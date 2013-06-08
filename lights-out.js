49f7b6762fd13251efd1c3b9f99827a1b84392b1
Marshall <mad.mod.max@gmail.com>
2013-06-08 00:22:33 -0400

    Level transitions working!
    When you get to the next level, well, that's a different story.

--- lights-out.js
+++ lights-out.js
@@ -7,17 +7,37 @@ $(document).ready(function() {
 
 	var W = $('#lights-out');
 
+	var currLevel = 0;
+	var levels = ["level-one", "level-two"];
+
 	Q.Sprite.extend("Player", {
 		init: function(p) {
 			this._super(p, {
-				asset: "player.png",
+				sheet: "player",
 				x: 64,
 				y: W.height() - 48
 			});
 
 			this.add("2d, platformerControls");
+
+			this.on("hit.sprite", function(collision) {
+				if (collision.obj.isA("Gateway")) {
+					Q.stageScene(levels[currLevel], 1);
+				}
+
+			})
 		}
 	});
+
+	Q.Sprite.extend("Gateway", {
+		init: function(p) {
+			this._super(p, { sheet: "gateway" })
+
+		}
+
+	})
+
+	//var player = new Q.Player();
 
 	Q.scene("level-one", function(stage) {
 		stage.insert(new Q.Repeater({ asset: "background.png", speedX: 0.5, speedY: 0.5}));
@@ -29,15 +49,37 @@ $(document).ready(function() {
 
 		var player = stage.insert(new Q.Player());
 
+		stage.insert(new Q.Gateway({ x: W.width() - 48, y: 144 }));
+
 		stage.add("viewport").follow(player, { x: true, y: true });
-		//stage.viewport.scale = 2;
+		stage.viewport.scale = 2;
+
+		currLevel += 1;
 	});
 
-	Q.load("player.png, tiles.png, level-one.json, background.png", function() {
+	Q.scene("level-two", function(stage) {
+		stage.insert(new Q.Repeater({ asset: "background.png", speedX: 0.5, speedY: 0.5}));
+
+		stage.collisionLayer(new Q.TileLayer({
+			dataAsset: "level-two.json",
+			sheet: "tiles"
+		}));
+
+		var player = stage.insert(new Q.Player());
+
+		//stage.insert(new Q.Gateway({ x: W.width() - 48, y: 144 }));
+
+		stage.add("viewport").follow(player, { x: true, y: true });
+		stage.viewport.scale = 2;
+	});
+
+	Q.load("sprites.png, sprites.json, tiles.png, level-one.json, level-two.json, background.png", function() {
 		Q.sheet("tiles", "tiles.png", { tilew: 32, tileh: 32 });
 
-		Q.compileSheets("tiles.png", "testlevel.json");
+		Q.compileSheets("sprites.png", "sprites.json");
+		Q.compileSheets("tiles.png", "level-one.json");
 
-		Q.stageScene("level-one");
+		Q.clearStages();
+		Q.stageScene(levels[currLevel]);
 	});
 });
