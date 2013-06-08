5963a04ed23b221730e480dc36068f62e2b3dc79
Marshall <mad.mod.max@gmail.com>
2013-06-07 20:04:05 -0400

    Collision with tile, after much frustration.

--- lights-out.js
+++ lights-out.js
@@ -1,19 +1,29 @@
-window.addEventListener("load", function() {
+$(document).ready(function() {
 	var Q = Quintus()
 		.include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
-		.setup()
+		.setup({ maximize: true })
 		.controls()
 		.touch();
 
 	Q.Sprite.extend("Player", {
 		init: function(p) {
 			this._super(p, {
-				x: 10,
-				y: 10,
-				asset: "player.png"
+				asset: "player.png",
+				x: 0,
+				y: 0
 			});
 
 			this.add("2d, platformerControls");
+		}
+	});
+
+	Q.Sprite.extend("Tile", {
+		init: function(p) {
+			this._super(p, {
+				asset: "tile.png",
+				x: 0,
+				y: 600,
+			});
 		}
 	});
 
@@ -21,15 +31,15 @@ window.addEventListener("load", function() {
 		stage.insert(new Q.Repeater({ asset: "background.png", speedX: 0.5, speedY: 0.5}));
 
 		var player = stage.insert(new Q.Player());
+		var tile = stage.insert(new Q.Tile());
 
 		stage.add("viewport").follow(player);
-
 	});
 
-	Q.load("player.png, background.png", function() {
+	Q.load("player.png, tile.png, background.png", function() {
 		var player = new Q.Player();
+		var tile = new Q.Tile();
 
 		Q.stageScene("testlevel");
-
 	});
 });
