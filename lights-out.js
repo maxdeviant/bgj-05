f0cc1238b59ba27cde14f8f13658b3b8fb004208
Marshall <mad.mod.max@gmail.com>
2013-06-08 09:37:44 -0400

    Added bouncing pad mechanic.

--- lights-out.js
+++ lights-out.js
@@ -26,22 +26,36 @@ $(document).ready(function() {
 					this.destroy();
 				}
 
-			})
+			});
 		}
 	});
 
 	Q.Sprite.extend("Gateway", {
 		init: function(p) {
-			this._super(p, { sheet: "gateway" })
-
+			this._super(p, { sheet: "gateway" });
 		}
+	});
 
-	})
+	Q.Sprite.extend("BouncePad", {
+		init: function(p) {
+			this._super(p, { sheet: "bounce-pad" });
+		
+			this.add("2d, aiBounce");
+
+
+			this.on("bump.top", function(collision) {
+				if (collision.obj.isA("Player")) {
+					collision.obj.p.vy = -500;
+				}
+
+			});
+		}
+	});
 
 	//var player = new Q.Player();
 
 	Q.scene("level-one", function(stage) {
-		stage.insert(new Q.Repeater({ asset: "background.png", speedX: 0.5, speedY: 0.5}));
+		stage.insert(new Q.Repeater({ asset: "background.png", speedX: 1, speedY: 1}));
 
 		stage.collisionLayer(new Q.TileLayer({
 			dataAsset: "level-one.json",
@@ -68,7 +82,7 @@ $(document).ready(function() {
 
 		var player = stage.insert(new Q.Player());
 
-		//stage.insert(new Q.Gateway({ x: W.width() - 48, y: 144 }));
+		stage.insert(new Q.BouncePad({ x: 128, y: W.height() - 48 }));
 
 		stage.add("viewport").follow(player, { x: true, y: true });
 		stage.viewport.scale = 2;
@@ -81,6 +95,7 @@ $(document).ready(function() {
 		Q.compileSheets("tiles.png", "level-one.json");
 
 		Q.clearStages();
-		Q.stageScene(levels[currLevel]);
+		//Q.stageScene(levels[currLevel]);
+		Q.stageScene("level-two");
 	});
 });
