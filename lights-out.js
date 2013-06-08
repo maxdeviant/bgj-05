3804b576e156d0bfbe832231a99c00330f65b834
Marshall <mad.mod.max@gmail.com>
2013-06-07 21:10:22 -0400

    Level generation is progressing nicely.

--- lights-out.js
+++ lights-out.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 		init: function(p) {
 			this._super(p, {
 				asset: "player.png",
-				x: 0,
-				y: 0
+				x: 64,
+				y: $('#lights-out').height() - 48
 			});
 
 			this.add("2d, platformerControls");
@@ -22,12 +22,25 @@ $(document).ready(function() {
 			this._super(p, {
 				asset: "tile.png",
 				x: 0,
-				y: 600
+				y: 0
 			});
 		}
 	});
 
+	Q.Sprite.extend("Air", {
+		init: function(p) {
+			this._super(p, {
+				asset: "air.png",
+				x: 0,
+				y: 0
+			});
+
+		}
+	});
+
 	Q.scene("testlevel", function(stage) {
+		stage.add("viewport");
+
 		stage.insert(new Q.Repeater({ asset: "background.png", speedX: 0.5, speedY: 0.5}));
 
 		var player = stage.insert(new Q.Player());
@@ -35,17 +48,22 @@ $(document).ready(function() {
 		
 		for (var i = 0; i < 30; i++) {
 			tiles[i] = stage.insert(new Q.Tile({
-				x: -1000 + 64 * i,
-				y: $('#lights-out').height() - 32
+				x: 16 + 32 * i,
+				y: $('#lights-out').height() - 16
 			}));
 		}
 
-		//var tile = stage.insert(new Q.Tile());
+		var air = stage.insert(new Q.Air({
+			x: 16,
+			y: 16
+		}));
 
-		stage.add("viewport").follow(player, { x: true, y: false });
+		//stage.add("viewport").follow(player, { x: true, y: false })''
+
+		//stage.add("viewport");
 	});
 
-	Q.load("player.png, tile.png, background.png", function() {
+	Q.load("player.png, tile.png, air.png, background.png", function() {
 		Q.stageScene("testlevel");
 	});
 });
